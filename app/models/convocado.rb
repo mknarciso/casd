798ca05fb0c9a69031786118ef6e6f24f164c5a1
mknarciso <murilokn@gmail.com>
2017-04-21 23:58:23 +0000

    Tags de bootstrap

--- app/models/convocado.rb
+++ app/models/convocado.rb
@@ -2,6 +2,14 @@ class Convocado < ActiveRecord::Base
     def self.import(file)
         CSV.foreach(file.path, col_sep: ";", headers: true) do |row|
             Convocado.create! row.to_hash
+            #hash = row.to_hash
+            #conv = Convocado.new row.to_hash
+            #if hash.especiais == "Sim"
+            #   hash.especiais = true
+            #else
+            #    hash.especiais = false
+            #end
+            #puts hash
         end
     end
 end
