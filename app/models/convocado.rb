25c5b2185a180b310c5e2b010ba9550a921c8662
mknarciso <murilokn@gmail.com>
2017-04-22 00:38:07 +0000

    date fix on import

--- app/models/convocado.rb
+++ app/models/convocado.rb
@@ -1,15 +1,12 @@
 class Convocado < ActiveRecord::Base
     def self.import(file)
         CSV.foreach(file.path, col_sep: ";", headers: true) do |row|
-            Convocado.create! row.to_hash
-            #hash = row.to_hash
+            #Convocado.create! row.to_hash
+            hash = row.to_hash
             #conv = Convocado.new row.to_hash
-            #if hash.especiais == "Sim"
-            #   hash.especiais = true
-            #else
-            #    hash.especiais = false
-            #end
-            #puts hash
+            hash["nascimento"] = Date.strptime(hash["nascimento"], '%d/%m/%y') 
+            hash["data_inscricao"] = DateTime.strptime(hash["data_inscricao"], '%d/%m/%y %H:%M') 
+            Convocado.create! hash
         end
     end
 end
