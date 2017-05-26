2863c3721120fc2f951e007b978ee02a1853d7a5
Marcos <marcosdp12@gmail.com>
2017-05-26 02:03:36 +0000

    user roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,30 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  def self.is_assistant
+    if self.role.eql? "assistant"
+      return true
+    end
+    return false
+  end
+  
+  def self.is_admin
+    if self.role.eql? "admin"
+      return true
+    end
+    return false
+  end
+  
+  def self.is_director
+    if self.role.eql? "director"
+      return true
+    end
+    return false
+  end
+  
+  def self.get_role
+    return self.role
+  end
+  
 end
