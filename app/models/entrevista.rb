61057341490b7eef6af4e3d97710156cd5d9461e
Gabriela Tourinho <gabrielantourinho@gmail.com>
2017-06-08 01:50:32 +0000

    Bug fix

--- app/models/entrevista.rb
+++ app/models/entrevista.rb
@@ -1,5 +1,6 @@
 class Entrevista < ActiveRecord::Base
     belongs_to :candidato
+    
     validates :entregou_documentos, presence: true
     validates :historico_escolar, presence: true
     validates :numero_de_pessoas, presence: true
@@ -9,7 +10,7 @@ class Entrevista < ActiveRecord::Base
     validates :aprovado, presence: true
     
     def nenhuma_renda?
-        aposentadorias.blank || auxilios.blank
+        aposentadorias.try(:empty?) || auxilios.try(:empty?)
     end
     
 end
