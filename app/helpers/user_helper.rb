2b1ea499c81869b65c2a11e13090738cff5954df
Marcos <marcosdp12@gmail.com>
2017-07-04 03:50:24 +0000

    correcoes

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,6 +1,6 @@
 module UserHelper
     def user_status(user)
-        if current_user.admin? || user.invitation_accepted?
+        if user.admin? || user.invitation_accepted?
             content_tag(:span, '', class: 'glyphicon glyphicon-ok color-sucess')
         else 
            'Convite Pendente'
